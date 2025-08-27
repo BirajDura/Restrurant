d411d5acb9d7d26f6227b1d1a5cc40d73d2ca4e4
Biraj Dura <durabiraj220@gmail.com>
2025-08-27 07:45:24 +0545

    updated

--- src/Client/Navbar.jsx
+++ src/Client/Navbar.jsx
@@ -2,13 +2,19 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-            <Link to="/" className="px-4 py-2 text-blue-500">Home</Link>
-            <Link to="/items" className="px-4 py-2 text-blue-500">Items</Link>
-            <Link to="/contact" className="px-4 py-2 text-blue-500">contact</Link>
-            <Link to="/ourteam" className="px-4 py-2 text-blue-500">Team</Link>
-
-        </div>
+        <nav className="flex items-center justify-between px-6 py-4 ">
+            <div className="text-2xl font-bold text-blue-500">
+                RESTRO
+            </div>
+            <div className="flex justify-center flex-grow">
+                <div className="flex space-x-6">
+                    <Link to="/" className="px-4 py-2 text-blue-500 hover:text-blue-700">Home</Link>
+                    <Link to="/items" className="px-4 py-2 text-blue-500 hover:text-blue-700">Items</Link>
+                    <Link to="/contact" className="px-4 py-2 text-blue-500 hover:text-blue-700">Contact</Link>
+                    <Link to="/ourteam" className="px-4 py-2 text-blue-500 hover:text-blue-700">Team</Link>
+                </div>
+            </div>
+        </nav>
     )
 }
 
