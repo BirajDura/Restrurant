37cffd1c88fefd3feef2d9c03fdc6f2912aa287d
Biraj Dura <durabiraj220@gmail.com>
2025-08-27 07:45:06 +0545

    updated

--- src/Client/Home.jsx
+++ src/Client/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Items from './Items'
 
 const Home = () => {
   return (
     <div>
       Welcome to the MoMo Restro.
+      <div>
+        <Items/>
+      </div>
     </div>
   )
 }
