ac8d53baa415667c8c05f25be67689fc4f19396b
Biraj Dura <durabiraj220@gmail.com>
2025-08-28 21:27:12 +0545

    updated

--- src/Client/OurTeam.jsx
+++ src/Client/OurTeam.jsx
@@ -4,7 +4,12 @@ const OurTeam = () => {
   return (
     <div>
       The best of us.
-      Welcome 
+      Welcome  
+      <li>Name</li>
+      <li>Address</li>
+      <li>Phone Number</li>
+      <li></li>
+
     </div>
   )
 }
