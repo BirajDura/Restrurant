2720211e336c3bc60abf530857aba178b4b80e5a
Biraj Dura <durabiraj220@gmail.com>
2025-08-29 20:40:52 +0545

    updated

--- src/Client/Navbar.jsx
+++ src/Client/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
             </div>
             <div className="flex justify-center flex-grow">
                 <div className="flex space-x-6">
-                    <Link to="/" className="px-4 py-2 text-blue-500 hover:text-blue-700">Home</Link>
-                    <Link to="/items" className="px-4 py-2 text-blue-500 hover:text-blue-700">Items</Link>
-                    <Link to="/contact" className="px-4 py-2 text-blue-500 hover:text-blue-700">Contact</Link>
-                    <Link to="/ourteam" className="px-4 py-2 text-blue-500 hover:text-blue-700">Team</Link>
+                    <Link to="/" className="px-4 py-2 text-xl text-blue-500 hover:text-blue-700">Home</Link>
+                    <Link to="/items" className="px-4 py-2 text-xl text-blue-500 hover:text-blue-700">Items</Link>
+                    <Link to="/contact" className="px-4 py-2 text-xl text-blue-500 hover:text-blue-700">Contact</Link>
+                    <Link to="/ourteam" className="px-4 py-2 text-xl text-blue-500 hover:text-blue-700">Team</Link>
                 </div>
             </div>
         </nav>
