b6d337412284fca0a08fd349a43bf1f066553451
Biraj Dura <durabiraj220@gmail.com>
2026-03-23 13:24:35 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";   // ← no BrowserRouter here
+
 import Navbar from "./Client/Navbar";
 import Home from "./Client/Home";
+import Items from "./Client/Items";          // your menu grid
+import ItemDetail from "./Client/ItemDetail"; // detail page
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* If your menu grid is shown directly on home, it should be inside Home.jsx */}
+        {/* If you want a separate /menu page: */}
+        {/* <Route path="/menu" element={<Items />} /> */}
+        
+        <Route path="/menu/:id" element={<ItemDetail />} />
+        
+        {/* Optional 404 catch-all */}
+        <Route path="*" element={<div className="text-white text-center py-20">404 - Page Not Found</div>} />
+      </Routes>
     </>
   );
 }
