8eb4ec2de21f1d0aff33b597ea5f0b4852df64d4
Biraj Dura <durabiraj220@gmail.com>
2026-03-23 13:21:37 +0545

    data folder added

---
+++ src/data/menudata.js
@@ -0,0 +1,59 @@
+import burger     from '../assets/ChickenBurger.jpg';
+import ramen      from '../assets/KoreanRamen.jpg';
+import pizza      from '../assets/MargheritaPizza.jpg';
+import chicken    from '../assets/CrispyFriedChicken.jpg';
+import pasta      from '../assets/CreamyPasta.jpg';
+import salad      from '../assets/FreshGardenSalad.jpg';
+import cake       from '../assets/ChocolateLavaCake.jpg';
+import milkshake  from '../assets/StrawberryMilkshake.jpg';
+
+export const menuItems = [
+  {
+    id: 'himalayan-chicken-burger',
+    name: 'Himalayan Chicken Burger',
+    image: burger,
+    desc: 'Grilled chicken patty layered with fresh lettuce, tomato, and creamy house sauce inside a toasted bun.'
+  },
+  {
+    id: 'spicy-korean-ramen',
+    name: 'Spicy Korean Ramen',
+    image: ramen,
+    desc: 'Hot and flavorful ramen noodles served with egg, vegetables, and rich spicy broth.'
+  },
+  {
+    id: 'classic-margherita-pizza',
+    name: 'Classic Margherita Pizza',
+    image: pizza,
+    desc: 'Wood-fired pizza topped with mozzarella cheese, fresh basil, and tomato sauce.'
+  },
+  {
+    id: 'crispy-fried-chicken',
+    name: 'Crispy Fried Chicken',
+    image: chicken,
+    desc: 'Golden crispy chicken pieces served with spicy dip and crunchy fries.'
+  },
+  {
+    id: 'creamy-white-pasta',
+    name: 'Creamy White Pasta',
+    image: pasta,
+    desc: 'Italian style penne pasta cooked in rich creamy Alfredo sauce with herbs.'
+  },
+  {
+    id: 'fresh-garden-salad',
+    name: 'Fresh Garden Salad',
+    image: salad,
+    desc: 'A healthy mix of lettuce, cucumber, tomato, olives, and lemon dressing.'
+  },
+  {
+    id: 'chocolate-lava-cake',
+    name: 'Chocolate Lava Cake',
+    image: cake,
+    desc: 'Warm chocolate cake with a rich molten chocolate center.'
+  },
+  {
+    id: 'strawberry-milkshake',
+    name: 'Strawberry Milkshake',
+    image: milkshake,
+    desc: 'Sweet strawberry blended milkshake topped with whipped cream.'
+  }
+];
