5f396371c6fbc36bac0cf2c7e584c56af36c6ffc
Biraj Dura <durabiraj220@gmail.com>
2026-03-23 13:25:01 +0545

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
+// src/main.jsx
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";   // ← only here
+
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <App />
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
